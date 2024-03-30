6d21689d21243646c5a7da9f7591c2047812e638
kiril <kirill551@yandex.ru>
2024-03-30 12:33:32 +0300

    "Project structure"

---
+++ ewm-service/src/main/java/ru/practicum/service/Service.java
@@ -0,0 +1,5 @@
+package ru.practicum.service;
+
+@SpringBootApplication
+public class Service {
+}
---
+++ stats-service/stats-dto/src/main/java/ru.practicum.statsDto/Dto.java
@@ -0,0 +1,10 @@
+package ru.practicum.statsDto;
+import lombok.Data;
+
+@Data
+public class Dto {
+    String x;
+    String y;
+
+
+}
