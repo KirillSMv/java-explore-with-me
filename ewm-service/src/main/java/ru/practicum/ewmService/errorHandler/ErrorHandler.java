acb76ac7e1a65b84b1b86dcf1599cf4e67155aec
kiril <kirill551@yandex.ru>
2024-04-10 23:32:21 +0300

    "Style"

--- ewm-service/src/main/java/ru/practicum/ewmService/errorHandler/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/ewmService/errorHandler/ErrorHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -104,6 +105,12 @@ public class ErrorHandler {
         return new ErrorResponse(e.getStackTrace().toString(), e.getLocalizedMessage(), e.getMessage(), LocalDateTime.now().format(TIME_PATTERN));
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handle(final MissingServletRequestParameterException e) {
+        log.error("MissingServletRequestParameterException:", e);
+        return new ErrorResponse(e.getStackTrace().toString(), e.getLocalizedMessage(), e.getMessage(), LocalDateTime.now().format(TIME_PATTERN));
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
