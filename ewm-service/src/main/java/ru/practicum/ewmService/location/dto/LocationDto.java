5a51cf910fb7539cb806f0733e4dfc5818bee91c
kiril <kirill551@yandex.ru>
2024-04-16 23:29:35 +0300

    "Feature: реализовать функциональность location_processing"

--- ewm-service/src/main/java/ru/practicum/ewmService/location/dto/LocationDto.java
+++ ewm-service/src/main/java/ru/practicum/ewmService/location/dto/LocationDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import ru.practicum.ewmService.location.validation.LatCoordinates;
 import ru.practicum.ewmService.location.validation.LonCoordinates;
 
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -17,7 +16,7 @@ import javax.validation.constraints.Size;
 public class LocationDto {
     private Long id;
 
-    @NotBlank(message = "name should not be blank")
+    @NotBlank(message = "name of location should not be blank")
     @Size(min = 4, max = 120, message = "name cannot has fewer characters than 4 and more than 120")
     private String name;
 
@@ -31,15 +30,4 @@ public class LocationDto {
 
     @NotNull(message = "please add \"rad\" value")
     private Float rad;
-
-    @AssertTrue(message = "Please check provided coordinates")
-    boolean areCoordinatesValid() {
-/*        if (lat < -90.0f || lat > 90.0f) {
-            throw new IllegalArgumentException("\"lat\" value cannot be fewer than -90 and more than 90");
-        }
-        if (lon < -180.0f || lon > 180.0f) {
-            throw new IllegalArgumentException("\"lon\" value cannot be fewer than -180 and more than 180");
-        }*/
-        return lat >= -90f && lat <= 90f && lon >= -180f && lon <= 180f;
-    }
 }
